cc0e90da0b76bfe7e7c98f7b0c85b917b34e394f
Darshanvadoliya <vadoliyadarshan34@gmail.com>
2025-02-25 15:37:28 +0530

    .

--- src/standard/standard.service.js
+++ src/standard/standard.service.js
@@ -1,88 +1,81 @@
 let { PrismaClient } = require('@prisma/client');
 const { lock } = require('./standard.routes');
-let prisma = new PrismaClient();    
+let prisma = new PrismaClient();
 
 // add standard
 let addStandard = async (data) => {
     let resData = {
-        status : 400,
-        data : {
+        status: 400,
+        data: {
             message: 'Error'
         }
     }
 
     try {
-
-        let addStandard = await prisma.standards.create({data:data})
-
+        let addStandard = await prisma.standards.create({ data: data })
         resData = {
-            status : 200,
-            data:{
-                message:'Standard add Success..',
-                data:addStandard
+            status: 200,
+            data: {
+                message: 'Standard add Success..',
+                data: addStandard
             }
         }
-
     } catch (error) {
         console.log(error);
     }
-
     return resData
 }
 
 // view standard
 let viewStandard = async (data) => {
     let resData = {
-        status : 400,
-        data : {
+        status: 400,
+        data: {
             message: 'Error'
         }
     }
-
     try {
-        let classCategory =  await prisma.classcategories.findFirst({
-            where:{
+        let classCategory = await prisma.classcategories.findFirst({
+            where: {
                 classcategories_nm: data.classCategory,
             },
-            include:{
+            include: {
                 standards: true
             }
         })
 
-        if(!data || Object.keys(data).length === 0){
+        if (!data || Object.keys(data).length === 0) {
             resData = {
-                status : 400,
-                data : {
+                status: 400,
+                data: {
                     message: 'class category not found'
                 }
             }
-        }else if(!classCategory){
+        } else if (!classCategory) {
             resData = {
-                status : 400,
-                data : {
+                status: 400,
+                data: {
                     message: 'class category not found'
                 }
             }
-        }else{
+        } else {
             let viewStandard = await prisma.standards.findMany({
-                where:{
+                where: {
                     categories_id: classCategory.id
                 },
-                select:{
+                select: {
                     standard: true
                 }
             })
 
-        resData = {
-            status : 200,
-            data:{
-                message:'Standard view Success..',
-                data:viewStandard
+            resData = {
+                status: 200,
+                data: {
+                    message: 'Standard view Success..',
+                    data: viewStandard
+                }
             }
         }
-        }
-
-        
 
     } catch (error) {
         console.log(error);
--- src/subjects/subject.service.js
+++ src/subjects/subject.service.js
@@ -4,49 +4,43 @@ let prisma = new PrismaClient()
 // add subject
 let addSubject = async (data) => {
     let resData = {
-        status : 400,
-        data : {
+        status: 400,
+        data: {
             message: 'Error'
         }
     }
-
     try {
-
-        let addSubject = await prisma.subjects.create({data:data})
-
+        let addSubject = await prisma.subjects.create({ data: data })
         resData = {
-            status : 200,
-            data:{
-                message:'Subject add Success..',
-                data:addSubject
+            status: 200,
+            data: {
+                message: 'Subject add Success..',
+                data: addSubject
             }
         }
 
     } catch (error) {
         console.log(error);
     }
-
     return resData
 }
-
 // view subject
 let viewSubject = async (data) => {
     let resData = {
-        status : 400,
-        data : {
+        status: 400,
+        data: {
             message: 'Error'
         }
     }
-
     try {
 
         let viewSubject = await prisma.subjects.findMany({})
-
+        
         resData = {
-            status : 200,
-            data:{
-                message:'Subject add Success..',
-                data:viewSubject
+            status: 200,
+            data: {
+                message: 'Subject add Success..',
+                data: viewSubject
             }
         }
 
